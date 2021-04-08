e7963b0fcc964b0972635db5ea53679818940aff
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-04-08 22:28:43 +0300

    feat: add solution

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -1,19 +1,14 @@
-/**
- * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
- * separated by hyphens.
- *
- * Your task is to check by given string inputString
- * whether it's a MAC-48 address or not.
- *
- * @param {Number} inputString
- * @return {Number}
- *
- * @example
- * For 00-1B-63-84-45-E6, the output should be true.
- *
- */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const str = String(n);
+  const arr = str.split('');
+  let res = true;
+  const mass = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', '-'];
+  for (let i = 0; i < arr.length; i++) {
+    if (mass.includes(arr[i]) !== true) {
+      res = false;
+    }
+  }
+  return res;
 }
 
 module.exports = isMAC48Address;
