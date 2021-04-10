dfcae48a6d8eb0e757f07fe6ba0f98ea4559dd18
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-04-10 16:06:55 +0300

    feat: add solution

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -1,28 +1,37 @@
-/**
- * In the popular Minesweeper game you have a board with some mines and those cells
- * that don't contain a mine have a number in it that indicates the total number of mines
- * in the neighboring cells. Starting off with some arrangement of mines
- * we want to create a Minesweeper game setup.
- *
- * @param {Array<Array>} matrix
- * @return {Array<Array>}
- *
- * @example
- * matrix = [
- *  [true, false, false],
- *  [false, true, false],
- *  [false, false, false]
- * ]
- *
- * The result should be following:
- * [
- *  [1, 2, 1],
- *  [2, 1, 1],
- *  [1, 1, 1]
- * ]
- */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const mass = [];
+  for (let i = 0; i < matrix.length; i++) {
+    const mass2 = [];
+    for (let j = 0; j < matrix[0].length; j++) {
+      let a = 0;
+      if (i > 0 && j > 0 && matrix[i - 1][j - 1] === true) {
+        a++;
+      }
+      if (i > 0 && matrix[i - 1][j] === true) {
+        a++;
+      }
+      if (i > 0 && j < (matrix[0].length - 1) && matrix[i - 1][j + 1] === true) {
+        a++;
+      }
+      if (j > 0 && matrix[i][j - 1] === true) {
+        a++;
+      }
+      if (j < (matrix[0].length - 1) && matrix[i][j + 1] === true) {
+        a++;
+      }
+      if (i < (matrix.length - 1) && j > 0 && matrix[i + 1][j - 1] === true) {
+        a++;
+      }
+      if (i < (matrix.length - 1) && matrix[i + 1][j] === true) {
+        a++;
+      }
+      if (i < (matrix.length - 1) && j < (matrix[0].length - 1) && matrix[i + 1][j + 1] === true) {
+        a++;
+      }
+      mass2.push(a);
+    }
+    mass.push(mass2);
+  }
+  return mass;
 }
-
 module.exports = minesweeper;
