4b607f025692c2d9cbb322813da0bf90c80690be
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-04-11 01:31:12 +0300

    Update 10-find-index.js

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -1,17 +1,1 @@
-function findIndex(array, value) {
-  let a = 0;
-  let b = array.length;
-  let i;
-  while (1) {
-    i = Math.floor((a + b) / 2);
-    if (array[i] === value) {
-      break;
-    } else if (array[i] > value) {
-      b = --i;
-    } else {
-      a = ++i;
-    }
-  }
-  return i;
-}
-module.exports = findIndex;
+
