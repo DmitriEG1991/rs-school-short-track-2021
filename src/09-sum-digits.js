96f87a30c2f568555b715cf72f4a107b6cb9f332
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-04-10 16:08:01 +0300

    feat: add solution

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -1,17 +1,15 @@
-/**
- * Given a number, replace this number with
- * the sum of its digits until we get to a one digit number.
- *
- * @param {Number} n
- * @return {Number}
- *
- * @example
- * For 100, the result should be 1 (1 + 0 + 0 = 1)
- * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
- *
- */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let str = String(n);
+  let res;
+  do {
+    let a = 0;
+    for (let i = 0; i < str.length; i++) {
+      a += Number(str[i]);
+    }
+    str = String(a);
+    res = a;
+  } while (str.length > 1);
+  return res;
 }
 
 module.exports = getSumOfDigits;
