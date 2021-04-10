ef600e17e2faf420308303bf027271b10866284a
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-04-10 16:11:50 +0300

    feat: add solution

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -1,15 +1,7 @@
-/**
- * Given an email address, return it's domain.
- *
- * @param {String} email
- * @return {String}
- *
- * @example
- * For the input 'prettyandsimple@example.com', the output should be 'example.com'
- *
- */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const mass = email.split('@');
+  const res = mass[mass.length - 1];
+  return res;
 }
 
 module.exports = getEmailDomain;
