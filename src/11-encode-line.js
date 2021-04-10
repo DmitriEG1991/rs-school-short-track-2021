96f5a9e1fff635073bdc124eb2007aab74f9e708
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-04-10 16:09:50 +0300

    feat: add solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -1,15 +1,17 @@
-/**
- * Given a string, return its encoding version.
- *
- * @param {String} str
- * @return {String}
- *
- * @example
- * For aabbbc should return 2a3bc
- *
- */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let res = '';
+  let a = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      a++;
+    } else if (str[i] !== str[i + 1] && a === 1) {
+      res += str[i];
+    } else {
+      res = res + String(a) + str[i];
+      a = 1;
+    }
+  }
+  return res;
 }
 
 module.exports = encodeLine;
